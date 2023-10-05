e33cc1cb9998aaa31c5be65bb99eb97bafb941ad
Cheng Shufen <ophelia.scarlett007@gmail.com>
2023-10-05 15:03:24 +0300

    change the guesschance-- and add post mapping

--- src/main/java/com/javagame/numberguess/GameController.java
+++ src/main/java/com/javagame/numberguess/GameController.java
@@ -2,11 +2,13 @@ package com.javagame.numberguess;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//get mapping
 @RequestMapping("/api")
 public class GameController {
 
@@ -18,8 +20,20 @@ public class GameController {
         gameService.restart();
         return "Game restarted! You have 5 new chance to guess the number between 0 and 20.";
     }
+    @PostMapping("/restart")
+    public String restartPost() {
+        gameService.restart();
+        return "Game restarted! You have 5 new chance to guess the number between 0 and 20.";
+    }
+
+    //guess cant link from postman but import my number is working????
     @GetMapping("/guess")
     public String guess(@RequestParam int number) {
+        return gameService.guess(number);
+    }
+    //http://localhost:8080/api/guess?number=<your-guess> this one works????why
+    @PostMapping("/guess")
+    public String guessPost(@RequestParam int number) {
         return gameService.guess(number);
     }
     
--- src/main/java/com/javagame/numberguess/GameService.java
+++ src/main/java/com/javagame/numberguess/GameService.java
@@ -15,12 +15,21 @@ public class GameService {
     public String guess(int number) {
         if (guessChance <= 0) {
             return "You have no more chance left. The number was " + numberToGuess + ". Game restarted! You have 5 new attempts to guess the number between 0 and 20.";
-        }else if (number == numberToGuess) {
-            return "Great job! You guessed the number! Game restarted! You have 5 new chances to guess the number between 0 and 20.";
+        }
+        //guessChance--;
+        else if (number == numberToGuess) {
+            return "Great Job! You guessed the number! Game restarted! You have 5 new chances to guess the number between 0 and 20.";
         } else if (number > numberToGuess) {
-            guessChance--;
+            guessChance--; 
+            if(guessChance == 0) {
+                return "You have no more chance left. The number was " + numberToGuess + ". Game restarted! You have 5 new attempts to guess the number between 0 and 20.";
+            }
             return "Your guess higher than secret number! You have " + guessChance + " chances left. Guess again";
         } else {
+            guessChance--;
+            if(guessChance == 0) {
+                return "You have no more chance left. The number was " + numberToGuess + ". Game restarted! You have 5 new attempts to guess the number between 0 and 20.";
+            }
             return "Your guess is too low! You have " + guessChance + " chances left.";
         }
     }
